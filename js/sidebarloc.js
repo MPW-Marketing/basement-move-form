8016071acc96d121912664f6dab00eb6504d8237
Damon Maldonado <damon@mpwmarketing.com>
2016-02-17 15:14:10 -0500

    basic plugin

---
+++ js/sidebarloc.js
@@ -0,0 +1,25 @@
+
+function jqMoveUpdateSize(){
+    // Get the dimensions of the viewport
+    var width = jQuery(window).width();
+    var height = jQuery(window).height();
+    return width;
+};
+var is_moved = false;
+jQuery(document).ready(function(){
+    var width = jqMoveUpdateSize();
+    if (width < 761){
+        console.log('mobile');
+        jQuery('.page-content p:nth-of-type(2)').prepend(jQuery('#text-3'));
+    	is_moved = true;
+    }
+    jQuery(window).resize(function() {
+        if (jqMoveUpdateSize() < 761 && !is_moved){
+        		jQuery('.page-content p:nth-of-type(2)').prepend(jQuery('#text-3'));
+    			is_moved = true;
+        } else if (jqMoveUpdateSize() > 760 && is_moved){
+        		jQuery('aside.right .sidebar-bg').prepend(jQuery('#text-3'));
+    			is_moved = false;
+        }
+    });
+});
